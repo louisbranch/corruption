ca055dfcc01ceaa31640233499e87bf949ce2873
Luiz Branco <me@luizbranco.com>
2012-12-16 14:28:52 -0200

    Cards can only be casted on the right turn

--- js/models/croupier.js
+++ js/models/croupier.js
@@ -29,12 +29,9 @@ define(['backbone', 'models/bank', 'models/cards', 'config'], function (Backbone
     },
 
     castCard: function (card) {
-      if ( !this.hand.include(card) ) {
-        throw 'Card must be in your hand';
-      }
-      if ( !this.bank.payCost(card.get('cost')) ) {
-        throw 'Not enough funds';
-      }
+      if (!this.hand.include(card)) { throw 'Card must be in your hand'; }
+      if (!this.bank.payCost(card.get('cost'))) { throw 'Not enough funds'; }
+      if (!this.isHisTurn()) { throw 'It is not your turn yet'; }
       this.hand.remove(card);
       this.table.add(card);
     },
@@ -44,12 +41,16 @@ define(['backbone', 'models/bank', 'models/cards', 'config'], function (Backbone
     },
 
     endTurn: function () {
-      if (this.player.game.isPlayerTurn(this.player)) {
+      if (this.isHisTurn()) {
         this.table.endTurn();
         this.player.game.nextTurn();
       } else {
         throw 'It is not your turn';
       }
+    },
+
+    isHisTurn: function () {
+      return this.player.game.isPlayerTurn(this.player);
     }
 
   })
