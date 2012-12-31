7a73d41de45e1b39be5a44dd303b606c06ea3805
Luiz Branco <me@luizbranco.com>
2012-12-31 15:22:35 -0200

    Set initial test suit

---
+++ test/main.js
@@ -0,0 +1,23 @@
+requirejs.config({
+  baseUrl: 'js',
+  paths: {
+    'jquery': 'lib/jquery-1.8.2',
+    'underscore': 'lib/underscore',
+    'backbone': 'lib/backbone',
+    'mustache': 'lib/mustache',
+    'text': 'lib/text'
+  },
+  shim: {
+    'underscore': {
+      exports: '_'
+    },
+    'backbone': {
+      deps: ['underscore', 'jquery'],
+      exports: 'Backbone'
+    }
+  }
+});
+QUnit.config.autostart = false;
+require(['/test/models/croupier.test.js'], function () {
+  QUnit.start();
+});
---
+++ test/models/croupier.test.js
@@ -0,0 +1,7 @@
+define(['/js/models/croupier.js'], function (Croupier) {
+
+  test('Test example', function () {
+    equal(1, 1, 'Just to make sure!');
+  });
+
+});
